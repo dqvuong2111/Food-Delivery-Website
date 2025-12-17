19ff50e621887adf6aca0fb533f9009842a5280b
Sang <emtenlasangfb@gmail.com>
2025-12-17 21:45:47 +0700

    ghi chú ở đây

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -78,4 +78,16 @@ const userOrders = async(req, res) => {
   }
 }
 
-export { placeOrder, verifyOrder, userOrders};
+//Listing orders for admin panel
+const listOrder=async(req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({success:true, data:orders})
+
+  }catch (error) {
+    console.log(error);
+    res.json({success:false, message:"Error"})
+  }
+}
+
+export { placeOrder, verifyOrder, userOrders,listOrder };
